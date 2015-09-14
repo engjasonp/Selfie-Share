583a6e224446f3847254150ff9be838bc21a8c3c
engjasonp <engjasonp@gmail.com>
2015-09-14 15:53:56 -0700

    Added 3 required methods to conform to MCSessionDelegate and MCBrowserViewControllerDelegate protocols.

--- Project25/ViewController.swift
+++ Project25/ViewController.swift
@@ -6,16 +6,25 @@
 //  Copyright (c) 2015 EngJason. All rights reserved.
 //
 
+import MultipeerConnectivity
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
     @IBOutlet weak var collectionView: UICollectionView!
+    
     var images = [UIImage]()
+    var peerID: MCPeerID!
+    var mcSession: MCSession!
+    var mcAdvertiserAssistant: MCAdvertiserAssistant!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Selfie Share"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: "importPicture")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "showConnectionPrompt")
+        peerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .Required)
+        mcSession.delegate = self
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -58,6 +67,37 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .ActionSheet)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .Default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .Default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+    
+    func startHosting(action: UIAlertAction!) {
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-project25", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant.start()
+    }
+    
+    func joinSession(action: UIAlertAction!) {
+        let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
+        mcBrowser.delegate = self
+        presentViewController(mcBrowser, animated: true, completion: nil)
+    }
+    
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
+        
+    }
+    
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
+        
+    }
+    
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
+        
     }
 
     override func didReceiveMemoryWarning() {
